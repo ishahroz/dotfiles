3ef2226dd45e75391b42c3fd1adaa47767bdd7ee
Shahroz Ahmad <ishahrozahmad90@gmail.com>
2025-01-06 02:22:47 -0400

    refactor: remove key mappings from init.lua and add slow quit functionality

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,55 +1,49 @@
--- Define the Hyper key as 'Capslock' (the Hyper key I set in Karabiner Elements)
-local hyper = {"cmd", "alt", "ctrl", "shift"}
+require "slowq"  -- Slow quit apps
 
--- Define the key mappings
-keyMappings = {
-    A = "Activity Monitor",
-    B = "Dbeaver",
-    C = "Cursor",
-    D = "Docker",
-    E = "",
-    F = "Finder",
-    G = "Google Chrome",
-    H = "Hammerspoon",
-    I = "IntelliJ IDEA",
-    J = "",
-    K = "",
-    L = "",
-    M = "Spotify",
-    N = "Notion Calendar",
-    O = "",
-    P = "PyCharm",
-    Q = "", -- Quit the current application (see below)
-    R = "", -- Reload Hammerspoon (see below)
-    S = "Slack",
-    T = "iTerm",
-    U = "",
-    V = "Visual Studio Code",
-    W = "WhatsApp",
-    X = "",
-    Y = "",
-    Z = "",
-    ["\\"] = "Google Chat",
-}
+-- -- Define the Hyper key as 'Capslock' (the Hyper key I set in Karabiner Elements)
+-- local hyper = {"cmd", "alt", "ctrl", "shift"}
 
--- Bind the Hyper key to the keyMappings
-for key, app in pairs(keyMappings) do
-    if app ~= "" then
-        hs.hotkey.bind(hyper, key, function()
-            hs.application.launchOrFocus(app)
-        end)
-    end
-end
+-- -- Define the key mappings
+-- keyMappings = {
+--     A = "Activity Monitor",
+--     B = "Dbeaver",
+--     C = "Cursor",
+--     D = "Docker",
+--     E = "",
+--     F = "Finder",
+--     G = "Google Chrome",
+--     H = "Hammerspoon",
+--     I = "IntelliJ IDEA",
+--     J = "",
+--     K = "",
+--     L = "",
+--     M = "Spotify",
+--     N = "Notion Calendar",
+--     O = "",
+--     P = "PyCharm",
+--     Q = "", -- Quit the current application (see below)
+--     R = "", -- Reload Hammerspoon (see below)
+--     S = "Slack",
+--     T = "iTerm",
+--     U = "",
+--     V = "Visual Studio Code",
+--     W = "WhatsApp",
+--     X = "",
+--     Y = "",
+--     Z = "",
+--     ["\\"] = "Google Chat",
+-- }
 
--- To quit the current application
-hs.hotkey.bind(hyper, "Q", function()
-    local app = hs.application.frontmostApplication()
-    if app then
-        app:kill()
-    end
-end)
+-- -- Bind the Hyper key to the keyMappings
+-- for key, app in pairs(keyMappings) do
+--     if app ~= "" then
+--         hs.hotkey.bind(hyper, key, function()
+--             hs.application.launchOrFocus(app)
+--         end)
+--     end
+-- end
 
--- To reload Hammerspoon configuration
-hs.hotkey.bind(hyper, "R", function()
-    hs.reload()
-end)
+-- -- To reload Hammerspoon configuration
+-- hs.hotkey.bind(hyper, "R", function()
+--     hs.reload()
+-- end)
