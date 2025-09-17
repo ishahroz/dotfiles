bd08b40e7e279bc0de8810e6cb99bc36ccee8927
Shahroz Ahmad <ishahrozahmad90@gmail.com>
2025-09-16 22:58:32 -0300

    feat: show hidden files in snack file finder and explorer

---
+++ config/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    -- To show the hidden files both in files finder and explorer
+    -- To learn about the configs: https://github.com/folke/snacks.nvim/blob/main/docs/picker.md
+    picker = {
+      sources = {
+        explorer = { hidden = true },
+        files = { hidden = true },
+      },
+    },
+  },
+}
